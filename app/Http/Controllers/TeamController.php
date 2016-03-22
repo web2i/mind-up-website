d7fefea48aa940e18fd6427885be8a72ae8a2488
Francois Vidogue <francois.vidogue@hotmail.fr>
2016-03-23 00:09:17 +0100

    Firsts changes Team List

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -11,9 +11,21 @@ class TeamController extends Controller
     protected function initData()
     {
         parent::initData();
+	//Titles
+	$this->data['h'][3][1] = 'DEFAULT'; // "Notre equipe"
+	$this->data['h'][3][2] = 'DEFAULT'; // name of member
+
+	//Paragraphes
+	$this->data['p'][1] = 'DEFAULT'; // "Voici notre equipe toujours prete a vous aider pour"....
+
+	//Span
+	$this->data['span'][1] = 'DEFAULT'; //job of member
+
+	//img
         $this->data['img']['profil'] = array(
                     'src' => $this->BASE_URL.'/ressources/profil.jpeg',
-                    'alt' => 'DEFAULT');
+                    'alt' => 'DEFAULT'); 
+
         $this->data['title'] = 'Home';
     }
 
--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -3,120 +3,120 @@
 @section('content')
 
 <section id="members">
-	<h3 class="members_title">Animated Grid Layout</h3>
-	<p class="members_text">A posuere donec senectus suspendisse bibendum magna ridiculus a justo orci parturient suspendisse ad rhoncus cursus ut parturient viverra elit aliquam ultrices est sem. Tellus nam ad fermentum ac enim est duis facilisis congue a lacus adipiscing consequat risus consectetur scelerisque integer suspendisse a mus integer elit massa ut.</p>
+	<h3 class="members_title">{{ $h[3][1] }}</h3>
+	<p class="members_text">{{ $p[1] }}</p>
 	<ul class="members_list">
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 	</ul>
 	<ul class="members_list">
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
-			</div>
-		</li>
-	</ul>
-	<ul class="members_list">
-		<li class="member">
-			<img src="ressources/profil.jpeg">
-			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="ressources/profil.jpeg">
-			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="ressources/profil.jpeg">
-			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="ressources/profil.jpeg">
-			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="ressources/profil.jpeg">
-			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 	</ul>
 	<ul class="members_list">
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 	</ul>
 	<ul class="members_list">
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 		<li class="member">
-			<img src="ressources/profil.jpeg">
+			<img src="{{ $img['profil']['src'] }}">
 			<div>
-				<h3>Parturient suspendisse</h3>
-				<span>fermentum ac enim est</span>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+	</ul>
+	<ul class="members_list">
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
 			</div>
 		</li>
 	</ul>
