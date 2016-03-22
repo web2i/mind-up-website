95be706a1943525c426c2c6d09e36a523f70c574
Francois Vidogue <francois.vidogue@hotmail.fr>
2016-03-23 00:45:21 +0100

    Changes Team List

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -5,6 +5,7 @@
 <section id="members">
 	<h3 class="members_title">{{ $h[3][1] }}</h3>
 	<p class="members_text">{{ $p[1] }}</p>
+	<h3 class="members-category">{{ $h[3][3] }}</h3>
 	<ul class="members_list">
 		<li class="member">
 			<img src="{{ $img['profil']['src'] }}">
@@ -14,66 +15,7 @@
 			</div>
 		</li>
 	</ul>
-	<ul class="members_list">
-		<li class="member">
-			<img src="{{ $img['profil']['src'] }}">
-			<div>
-				<h3>{{ $h[3][2] }}</h3>
-				<span>{{ $span[1] }}</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="{{ $img['profil']['src'] }}">
-			<div>
-				<h3>{{ $h[3][2] }}</h3>
-				<span>{{ $span[1] }}</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="{{ $img['profil']['src'] }}">
-			<div>
-				<h3>{{ $h[3][2] }}</h3>
-				<span>{{ $span[1] }}</span>
-			</div>
-		</li>
-	</ul>
-	<ul class="members_list">
-		<li class="member">
-			<img src="{{ $img['profil']['src'] }}">
-			<div>
-				<h3>{{ $h[3][2] }}</h3>
-				<span>{{ $span[1] }}</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="{{ $img['profil']['src'] }}">
-			<div>
-				<h3>{{ $h[3][2] }}</h3>
-				<span>{{ $span[1] }}</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="{{ $img['profil']['src'] }}">
-			<div>
-				<h3>{{ $h[3][2] }}</h3>
-				<span>{{ $span[1] }}</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="{{ $img['profil']['src'] }}">
-			<div>
-				<h3>{{ $h[3][2] }}</h3>
-				<span>{{ $span[1] }}</span>
-			</div>
-		</li>
-		<li class="member">
-			<img src="{{ $img['profil']['src'] }}">
-			<div>
-				<h3>{{ $h[3][2] }}</h3>
-				<span>{{ $span[1] }}</span>
-			</div>
-		</li>
-	</ul>
+	<h3 class="members-category">{{ $h[3][3] }}</h3>
 	<ul class="members_list">
 		<li class="member">
 			<img src="{{ $img['profil']['src'] }}">
@@ -97,6 +39,69 @@
 			</div>
 		</li>
 	</ul>
+	<h3 class="members-category">{{ $h[3][3] }}</h3>
+	<ul class="members_list">
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+	</ul>
+	<h3 class="members-category">{{ $h[3][3] }}</h3>
+	<ul class="members_list">
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+		<li class="member">
+			<img src="{{ $img['profil']['src'] }}">
+			<div>
+				<h3>{{ $h[3][2] }}</h3>
+				<span>{{ $span[1] }}</span>
+			</div>
+		</li>
+	</ul>
+	<h3 class="members-category">{{ $h[3][3] }}</h3>
 	<ul class="members_list">
 		<li class="member">
 			<img src="{{ $img['profil']['src'] }}">
