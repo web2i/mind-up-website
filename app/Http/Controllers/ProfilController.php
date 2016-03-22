3f6418041ed58378038a3249309cd85d8790c904
loicbourgois <loicbourgois.web@gmail.com>
2016-03-22 23:29:41 +0100

    Add user array for profil page based on id

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -3,19 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\MyClasses\User;
 
 class ProfilController extends Controller
 {
-    protected function initData()
+    protected function initDataUser($user)
     {
-        parent::initData();
-        $this->data['title'] = $this->getText('profil');
+        $this->data['title'] = $user->getFirstname() . " " . $user->getName();
+        $this->data['user']['firstname'] = $user->getFirstname();
+        $this->data['user']['name'] = $user->getName();
+        $this->data['user']['job'] = $this->getText($user->getJobTextId());
+        $this->data['user']['email'] = $user->getEmail();
+        $this->data['user']['mobile'] = $user->getMobile();
+        echo '<pre>'; print_r($this->data['user']); echo '</pre>';
     }
 
     public function main($id)
     {
         $this->init();
         $this->initData();
+        $user = User::createFromId($id);
+        
+        
+        $this->initDataUser($user);
         return view('profil', $this->data);
     }
 }
