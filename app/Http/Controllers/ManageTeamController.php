bef25e7810089a912aac48f514ea35c53c74aaad
Francois Vidogue <francois.vidogue@hotmail.fr>
2016-03-23 21:52:55 +0100

    Beginning manage Team

--- app/Http/Controllers/ManageTeamController.php
+++ app/Http/Controllers/ManageTeamController.php
@@ -9,6 +9,9 @@ class ManageTeamController extends Controller
     protected function initData()
     {
         parent::initData();
+	//Titles
+	    $this->data['h'][3][1] = 'DEFAULT'; // "Gestion des utilisateurs"	
+
         $this->data['title'] = $this->getText('manage-team');
     }
 
--- resources/views/manage-team.blade.php
+++ resources/views/manage-team.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <section id="manage-team">
-	
+	<h3 class="manage_team_title">{{ $h[3][1] }}</h3>
 </section>
 
 @stop
