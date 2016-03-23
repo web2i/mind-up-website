56368c40fc4d80e7a437c8dccd118476f60ccd85
mdesmarchelier <azamanama@gmail.com>
2016-03-23 15:07:50 +0100

    Add text to project and img

--- sql/project_picture.sql
+++ sql/project_picture.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 23 Mars 2016 à 14:36
+-- Généré le :  Mer 23 Mars 2016 à 15:06
 -- Version du serveur :  5.5.47-0+deb8u1
 -- Version de PHP :  5.6.17-0+deb8u1
 
@@ -29,17 +29,23 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `project_picture` (
 `id` int(11) NOT NULL,
   `projectId` int(11) NOT NULL,
-  `picturePath` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `picturePath` varchar(255) NOT NULL,
+  `id_img` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `project_picture`
 --
 
-INSERT INTO `project_picture` (`id`, `projectId`, `picturePath`) VALUES
-(1, 1, 'picture1'),
-(2, 1, 'picture2'),
-(3, 1, 'picture3');
+INSERT INTO `project_picture` (`id`, `projectId`, `picturePath`, `id_img`) VALUES
+(1, 1, 'path', 'project-image-1'),
+(2, 1, 'path', 'project-image-2'),
+(3, 1, 'path', 'project-image-3'),
+(4, 1, 'path', 'project-tech-1'),
+(5, 1, 'path', 'project-tech-2'),
+(6, 1, 'path', 'project-tech-3'),
+(7, 1, 'path', 'profil-image-1'),
+(8, 1, 'path', 'profil-image-2');
 
 --
 -- Index pour les tables exportées
@@ -59,7 +65,7 @@ ALTER TABLE `project_picture`
 -- AUTO_INCREMENT pour la table `project_picture`
 --
 ALTER TABLE `project_picture`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
