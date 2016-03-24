02f358889d1409687a5dbf99cb6f0639305e1683
Francois Vidogue <francois.vidogue@hotmail.fr>
2016-03-24 19:36:56 +0100

    Begin inputs forms Contact and CSS

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -1,19 +1,19 @@
 function formInfos()
 {
-	alert("loic le pd");
+	
 }
 
 function formDevis()
 {
-	alert("loic l'homo");
+	
 }
 
 function formRendezVous()
 {
-	alert("loic le carnivore");
+	
 }
 
 function formAutre()
 {
-	alert("loic l'homme lama");
+	
 }
--- resources/views/contact-form.blade.php
+++ resources/views/contact-form.blade.php
@@ -5,10 +5,19 @@
 <section id="contact-form">
 	<h3 class="contact_form_title">{{ $h[3][1] }}</h3>
 	<div>
-		<input type="button" value="{{ $input[1] }}" onclick="formInfos()"> 
-		<input type="button" value="{{ $input[2] }}" onclick="formDevis()"> 
-		<input type="button" value="{{ $input[3] }}" onclick="formRendezVous()">
-		<input type="button" value="{{ $input[4] }}" onclick="formAutre()">
+		<input type="button" class="" value="{{ $input[1] }}" onclick="formInfos()"> 
+		<input type="button" class="" value="{{ $input[2] }}" onclick="formDevis()"> 
+		<input type="button" class="" value="{{ $input[3] }}" onclick="formRendezVous()">
+		<input type="button" class="" value="{{ $input[4] }}" onclick="formAutre()">
+	</div>
+	<div id="contact_form_area">
+		<form action="" method="POST">
+			<label class="contact_form_label2">Nom: </label><input class="contact_form_input" name="name" value="" /> <br/>
+			<label class="contact_form_label2">Prenom: </label><input class="contact_form_input" name="firstname" value="" /> <br/>
+			<label class="contact_form_label2">Nom entreprise: </label><input class="contact_form_input" name="company" value="" /> <br/>
+			<label class="contact_form_label2">Adresse mail: </label><input class="contact_form_input" name="email" value="" /> <br/>
+			<label class="contact_form_label2">Numero de Téléphone:</label><input class="contact_form_input" name="phone" value="" /> <br/>
+		</form>
 	</div>
 	
 </section>
