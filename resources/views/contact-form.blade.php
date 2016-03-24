9fe22f84742a26557685bf28c527acf1f37450a3
Francois Vidogue <francois.vidogue@hotmail.fr>
2016-03-24 20:52:41 +0100

    First Form of the page Contact

--- resources/views/contact-form.blade.php
+++ resources/views/contact-form.blade.php
@@ -11,13 +11,16 @@
 		<input type="button" class="" value="{{ $input[4] }}" onclick="formAutre()">
 	</div>
 	<div id="contact_form_area">
+		<table>
 		<form action="" method="POST">
-			<label class="contact_form_label2">Nom: </label><input class="contact_form_input" name="name" value="" /> <br/>
-			<label class="contact_form_label2">Prenom: </label><input class="contact_form_input" name="firstname" value="" /> <br/>
-			<label class="contact_form_label2">Nom entreprise: </label><input class="contact_form_input" name="company" value="" /> <br/>
-			<label class="contact_form_label2">Adresse mail: </label><input class="contact_form_input" name="email" value="" /> <br/>
-			<label class="contact_form_label2">Numero de Téléphone:</label><input class="contact_form_input" name="phone" value="" /> <br/>
+			<tr><td class="contact_form_td"><label class="contact_form_label2">Nom: </label></td><td  class="contact_form_td"><input class="contact_form_input" name="name" value="" /> </td></tr>
+			<tr><td  class="contact_form_td"><label class="contact_form_label2">Prenom: </label></td><td  class="contact_form_td"><input class="contact_form_input" name="firstname" value="" /> </td></tr>
+			<tr><td  class="contact_form_td"><label class="contact_form_label2">Nom entreprise: </label></td><td  class="contact_form_td"><input class="contact_form_input" name="company" value="" /></td></tr>
+			<tr><td  class="contact_form_td"><label class="contact_form_label2">Adresse mail: </label></td><td  class="contact_form_td"><input class="contact_form_input" name="email" value="" /> </td></tr>
+			<tr><td  class="contact_form_td"><label class="contact_form_label2">Numero de Téléphone:</label></td><td  class="contact_form_td"><input class="contact_form_input" name="phone" value="" /> </td></tr>
+			<tr><td  class="contact_form_td"><label class="contact_form_label2">Message:</label></td><td  class="contact_form_td"><textarea class="contact_form_textArea" name="textContact" ></textarea></td></tr>
 		</form>
+		<table>
 	</div>
 	
 </section>
