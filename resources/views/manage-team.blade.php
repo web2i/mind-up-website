a75d7a45ab9fcd2f1a1dd6aa3114a10293708898
Francois Vidogue <francois.vidogue@hotmail.fr>
2016-03-24 16:30:09 +0100

    Add form and dynamic checkbox id for Manage Team

--- resources/views/manage-team.blade.php
+++ resources/views/manage-team.blade.php
@@ -4,6 +4,8 @@
 
 <section id="manage-team">
 	<h3 class="manage_team_title">{{ $h[3][1] }}</h3>
+	
+	<form action="" method="post"> <!-- Action a definir -->
 	<ul class="manage_team_list">
 		<?php
 			echo '<table>';	
@@ -28,7 +30,7 @@
 				echo '<td class="manage_team_td">';
 				if($member['important']==0)
 				{
-					echo '<input type="checkbox">';
+					echo '<input id="important'.$member['id'].'" type="checkbox">';
 				}
 				else
 				{
@@ -39,8 +41,9 @@
 			}
 			echo '</table>';	
 		?>
-	<input type="button" id="manageTeamButton" value="{{ $input[1]}}">
+	<input type="button" id="manageTeamButton" type="submit" value="{{ $input[1]}}">
 	</ul>
+	</form>
 </section>
 
 @stop
