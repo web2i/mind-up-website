9326f2051872d864b8face72d4861ea625bcafdc
Francois Vidogue <francois.vidogue@hotmail.fr>
2016-03-25 10:39:14 +0100

    Table changes in Admin page

--- resources/views/manage-jobs.blade.php
+++ resources/views/manage-jobs.blade.php
@@ -13,25 +13,36 @@
 				<h3>Gestion droits/classes</h3>
 				<p>A posuere donec Tellus nam ad fermentum ac enim est duis facilisis congue a lacus adipiscing consequat risus consectetur scelerisque integer suspendisse a mus integer elit massa ut.senectus suspendisse bibendum magna ridiculus a justo orci parturient suspendisse ad rhoncus cursus ut parturient viverra elit aliquam ultrices est sem. Tellus nam ad fermentum ac enim est duis facilisis congue a lacus adipiscing consequat risus consectetur scelerisque integer suspendisse a mus integer elit massa ut.Tellus nam ad fermentum ac enim est duis facilisis congue a lacus adipiscing consequat risus consectetur scelerisque integer suspendisse a mus integer elit massa ut.</p>
 			</div>
-			
-<table>
+<div class="contTable">			
+<table id="jobsTable">
 <?php
-    echo '<tr><th></th>';
+    
+    echo '<tr class="jobsTableTh"><th>Order</th>';
     foreach($rights as $right) {
-        echo '<th>' . $right . '</th>';
+        echo '<th class="jobsTableTh">' . $right . '</th>';
     }
     echo '</tr>';
     foreach ($jobs as $job){
-        echo '<tr>';
+	$countTd=0;
+        echo '<tr class="jobsTableTr">';
         foreach($job as $aa){
-            echo '<td>' . $aa . '</td>';
+	    if($countTd >= 3)
+	    {
+            	echo '<td class="jobsTableTd">' . $aa . '</td>';
+	    }
+	    else
+	    {
+		echo '<td class="jobsTableTd2">' . $aa . '</td>';
+		$countTd=$countTd + 1;
+	    }
         }
         echo '</tr>';
+	
     }
 ?>
 </table>
-			
-			
+</div>		
+			<!--
 			<table class="admin">
 					<tr>
 					
@@ -43,7 +54,7 @@
 						<th>Communication</th>
 						<th>Prospection</th>
 					</tr>
-					<tr> <!-- Tr c'est la ligne et TD c'est les colonnes-->
+					<tr> 
 						<td>Droit 1</td>
 						<td><input type="checkbox" name="num_lignei" value="1"></td>
 						<td><input type="checkbox" name="num_lignei" value="1"></td>
@@ -89,9 +100,9 @@
 						<td><input type="checkbox" name="num_lignei" value="1"></td>
 						<td><input type="checkbox" name="num_lignei" value="1"></td>
 					</tr>
-				</table>
+				</table> -->
 
-				<table class="admin2">
+				<!--<table class="admin2">
 					<tr>
 					
 						<th>Inscrits\Classes</th>
@@ -102,7 +113,7 @@
 						<th>Communication</th>
 						<th>Prospection</th>
 					</tr>
-					<tr> <!-- Tr c'est la ligne et TD c'est les colonnes-->
+					<tr> 
 						<td>Nom 1</td>
 						<td><input type="checkbox" name="num_lignei" value="1"></td>
 						<td><input type="checkbox" name="num_lignei" value="1"></td>
@@ -148,9 +159,9 @@
 						<td><input type="checkbox" name="num_lignei" value="1"></td>
 						<td><input type="checkbox" name="num_lignei" value="1"></td>
 					</tr>
-				</table>
+				</table>-->
 				
-				<img class="add_user" src="ressources/add_user.png" alt="Nouveau !" />
+				<!--<img class="add_user" src="ressources/add_user.png" alt="Nouveau !" /> -->
 		</div>
 		
 		
