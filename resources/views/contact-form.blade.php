dd06eca8dfbf286093e0983ec09c7bb2488158af
Francois Vidogue <francois.vidogue@hotmail.fr>
2016-03-25 08:25:32 +0100

    Change labels in forms Contact

--- resources/views/contact-form.blade.php
+++ resources/views/contact-form.blade.php
@@ -15,27 +15,27 @@
 		<caption class="formsCaption">{{ $h['1'] }}</caption>
 		<form action="" method="POST">
 			<tr>
-				<td class="contact_form_td"><label class="contact_form_label2">Nom: </label></td>
+				<td class="contact_form_td"><label class="contact_form_label2">{{ $label['name'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="nameInfos" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Prenom: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['firstname'] }}  </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="firstnameInfos" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Nom entreprise: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['companyname'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="companyInfos" value="" /></td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Adresse mail: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['mail'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="emailInfos" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Numero de Téléphone:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['phone'] }}</label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="phoneInfos" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Message:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['message'] }}</label></td>
 				<td  class="contact_form_td"><textarea class="contact_form_textArea" name="textContactInfos" ></textarea></td>
 			</tr>
 			<tr>
@@ -50,43 +50,43 @@
 		<caption class="formsCaption">{{ $h['2'] }}</caption>
 		<form action="" method="POST">
 			<tr>
-				<td class="contact_form_td"><label class="contact_form_label2">Nom: </label></td>
+				<td class="contact_form_td"><label class="contact_form_label2">{{ $label['name'] }}  </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="nameDevis" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Prenom: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['firstname'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="firstnameDevis" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Nom entreprise: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['companyname'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="companyDevis" value="" /></td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Adresse mail: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['mail'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="emailDevis" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Numero de Téléphone:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['phone'] }}</label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="phoneDevis" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Budget aloué:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['budget'] }}</label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="BudgetDevis" /></td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Durée de développement:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['devTime'] }}</label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="DureeDeveloppementDevis" /></td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Date limite devis:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['deadline'] }}</label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="DateLimiteDevis" /></td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Nom du projet:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['projectName'] }}</label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="ProjectNameDevis" /></td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Description:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['description'] }}</label></td>
 				<td  class="contact_form_td"><textarea class="contact_form_textArea" name="textContactDevis" ></textarea></td>
 			</tr>
 			<tr>
@@ -100,31 +100,31 @@
 		<caption class="formsCaption">{{ $h['3'] }}</caption>
 		<form action="" method="POST">
 			<tr>
-				<td class="contact_form_td"><label class="contact_form_label2">Nom: </label></td>
+				<td class="contact_form_td"><label class="contact_form_label2">{{ $label['name'] }}  </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="nameRdv" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Prenom: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['firstname'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="firstnameRdv" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Nom entreprise: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['companyname'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="companyRdv" value="" /></td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Adresse mail: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['mail'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="emailRdv" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Numero de Téléphone:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['phone'] }}</label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="phoneRdv" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Message:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['message'] }}</label></td>
 				<td  class="contact_form_td"><textarea class="contact_form_textArea" name="textContactRdv" ></textarea></td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Dates de rendez-vous:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['appointment'] }}</label></td>
 				<td  class="contact_form_td"><!-- calendrier--></td>
 			</tr>
 			<tr>
@@ -138,27 +138,27 @@
 		<caption class="formsCaption">{{ $h['4'] }}</caption>
 		<form action="" method="POST">
 			<tr>
-				<td class="contact_form_td"><label class="contact_form_label2">Nom: </label></td>
+				<td class="contact_form_td"><label class="contact_form_label2">{{ $label['name'] }}</label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="nameAutre" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Prenom: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['firstname'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="firstnameAutre" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Nom entreprise: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['companyname'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="companyAutre" value="" /></td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Adresse mail: </label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['mail'] }} </label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="emailAutre" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Numero de Téléphone:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['phone'] }}</label></td>
 				<td  class="contact_form_td"><input class="contact_form_input" name="phoneAutre" value="" /> </td>
 			</tr>
 			<tr>
-				<td  class="contact_form_td"><label class="contact_form_label2">Message:</label></td>
+				<td  class="contact_form_td"><label class="contact_form_label2">{{ $label['message'] }}</label></td>
 				<td  class="contact_form_td"><textarea class="contact_form_textArea" name="textContactAutre" ></textarea></td>
 			</tr>
 			<tr>
