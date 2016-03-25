71757c864a745a3b4b563b099bd2eb3659a81d9b
loicbourgois <loicbourgois.web@gmail.com>
2016-03-25 08:22:01 +0100

    Add texts

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,21 +9,21 @@ class ContactController extends Controller
     protected function initData()
     {
         parent::initData();
-	//Buttons
-	$this->data['input']['infos'] = $this->getText('infos'); 
-	$this->data['input']['quotation'] = $this->getText('quotation'); 
-	$this->data['input']['appointment'] = $this->getText('appointment'); 
-	$this->data['input']['other'] = $this->getText('other'); 
+	    //Buttons
+	    $this->data['input']['infos'] = $this->getText('infos'); 
+	    $this->data['input']['quotation'] = $this->getText('quotation'); 
+	    $this->data['input']['appointment'] = $this->getText('appointment'); 
+	    $this->data['input']['other'] = $this->getText('other'); 
 
-	//Titles
+	    //Titles
 	
-	$this->data['h']['contact-us'] = $this->getText('contact-us'); // Nous contacter
-	$this->data['h']['1'] = 'Demande de renseignements'; // TODO
-	$this->data['h']['2'] = 'Demande de devis'; // TODO
-	$this->data['h']['3'] = 'Demande de rendez-vous'; // TODO
-	$this->data['h']['4'] = 'Autre demande'; // TODO
+	    $this->data['h']['contact-us'] = $this->getText('contact-us');
+	    $this->data['h']['1'] = $this->getText('contact-ask-infos');
+	    $this->data['h']['2'] = $this->getText('contact-ask-quotation');
+	    $this->data['h']['3'] = $this->getText('contact-ask-appointment');
+	    $this->data['h']['4'] = $this->getText('contact-ask-other');
 
-	$this->data['input'][1] = 'Envoyer ma demande'; // TODO
+	    $this->data['input'][1] = $this->getText('contact-button');
 
 
         $this->data['title'] = $this->getText('contact');
